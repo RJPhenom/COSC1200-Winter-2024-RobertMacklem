70e3432a22cdf20927e2e95d9c458a14c7c09791
RJ <rj@macklem.ca>
2024-01-26 15:35:07 -0500

    Update oops_concepts.java
    Updates oops_concepts.java with try-catch and while loops to validate user input in main program

--- Lab 2/oops_concepts.java
+++ Lab 2/oops_concepts.java
@@ -11,27 +11,52 @@ import java.util.Scanner;
 public class oops_concepts {
     //MAIN
     public static void main(String[] args) {
-        //Construct scanner to gather input
+        //Construct scanner to gather input and prep instance vars
         Scanner scanner = new Scanner(System.in);
+        Student student = null;
         
-        //Gather user input for the student information
-        System.out.println("Please enter the first name of the student:");
-        String fName = scanner.nextLine();
-        System.out.println("Please enter the last name of the student:");
-        String lName = scanner.nextLine();
+        Boolean lNameValid = false;
+        while (!lNameValid) {
+            try {
+                //Gather user input for the student information
+                System.out.println("Please enter the first name of the student:");
+                String fName = scanner.nextLine();
+                System.out.println("Please enter the last name of the student:");
+                String lName = scanner.nextLine();
 
-        // Create an instance of Student using the scanner inputs above
-        Student student = new Student(fName, lName);
+                //Fill our student instance using the scanner inputs above
+                student = new Student(fName, lName);
+                lNameValid = true;
+            } 
+            
+            catch (Exception e) {
+                //Handle exception on invalid input
+                System.out.println("\nINVALID INPUT: Last name was NULL. \nPlease enter a last name.\n");
+            }
+        }
 
-        // Access the information using getter methods
+        //Access the information using getter methods
         System.out.println("Name: " + student.getName());
         System.out.println("Age: " + student.getAge());
 
-        // Modify the student's information using setter methods
-        System.out.println("Now please update the age:");
-        student.setAge(scanner.nextInt());
+        //Set the age based on user input, using while loop and boolean to test for
+        //input validation.
+        Boolean ageValid = false;
+        while (!ageValid) {
+            try {
+                //Modify the student's information using setter methods
+                System.out.println("Now please update the age:");
+                student.setAge(scanner.nextInt());
+                ageValid = true;
+            } 
+            
+            catch (Exception e) {
+                //Handle exception on invalid input
+                System.out.println("\nINVALID INPUT: Age was out of range. \nPlease enter an age within the range 0-100.\n");        
+            }
+        }
 
-        // Print the updated information
+        //Print the updated information
         System.out.println("Student Name: " + student.getName());
         System.out.println("Student Age: " + student.getAge());
 
