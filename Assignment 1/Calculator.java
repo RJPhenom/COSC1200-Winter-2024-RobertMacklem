9f8511d7413f1a217cbd06282f94be0fb0b1f6af
RJ <rj@macklem.ca>
2024-01-27 18:21:02 -0500

    Update Calculator.java
    Added functionality of the calculator file for the assignment to Calculator.java. Now should just need to add validation to each 3 files.

--- Assignment 1/Calculator.java
+++ Assignment 1/Calculator.java
@@ -6,7 +6,38 @@
 //
 //          Text
 //
+import java.util.Scanner;
 
 public class Calculator {
-    
+    //MAIN
+    public static void main(String[] args) {
+        //Instantiat ethe scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        //Get the two numbers from user input
+        //--First Num--
+        System.out.println("Enter the first number: ");
+        Double num1 = scanner.nextDouble();
+
+        //--Second Num--
+        System.out.println("Enter the second number: ");
+        Double num2 = scanner.nextDouble();
+
+        //Perform Math operations
+        Double sum = num1 + num2;
+        Double diff = num1 - num2;
+        Double prod = num1 * num2;
+        Double quot = num1 / num2;
+        Double mod = num1 % num2;
+
+        //Print results
+        System.out.println(String.format("Sum: %f", sum));
+        System.out.println(String.format("Difference: %f", diff));
+        System.out.println(String.format("Product: %f", prod));
+        System.out.println(String.format("Quotient: %f", quot));
+        System.out.println(String.format("Remainder: %f", mod));
+
+        //Close scanner to prevent leak
+        scanner.close();
+    }
 }
