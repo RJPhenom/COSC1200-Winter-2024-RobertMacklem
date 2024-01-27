fbc4bdea80fbc60abe32905f567d052d56899fc4
RJ <rj@macklem.ca>
2024-01-27 17:09:49 -0500

    Update Person.java
    Fix: integer formatting in Person.introduce()
    
    +
    
    Add: main() functionality to take in user input, instantiate a person class, and output results using the introduce() method

--- Assignment 1/Person.java
+++ Assignment 1/Person.java
@@ -6,8 +6,43 @@
 //
 //          Text
 //
+import java.util.Scanner;
 
 public class Person {
+    //MAIN
+    public static void main(String[] args) {
+        //Instantiate input scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Print request for user input and store it as vars to
+        //build a Person instance later.
+        //--Name--
+        System.out.println("Enter your name: ");
+        String myName = scanner.nextLine();
+
+        //--Age--
+        System.out.println("Enter your age: ");
+        Integer myAge = scanner.nextInt();
+
+        //Consumes the \n left after nextInt(), which caused the next nextLine() to break 
+        //(Source: https://stackoverflow.com/questions/23450524/java-scanner-doesnt-wait-for-user-input)
+        scanner.nextLine(); 
+
+        //--Gender
+        System.out.println("Enter your gender (M, F, or X): ");
+        String myGenderStr = scanner.nextLine();
+        Character myGender = myGenderStr.charAt(0);
+
+        //Instantiate a Person using the inputs
+        Person myPerson = new Person(myName, myAge, myGender);
+
+        //Output results using introduce method
+        System.out.println(myPerson.introduce());
+
+        //Close scanner
+        scanner.close();
+    }
+
     //Attributes
     private String name;
     private Integer age;
@@ -55,7 +90,7 @@ public class Person {
         //Adds each line of ouput to the return string using the format method 
         //to return the value of the field in the given instance.
         returnMessage += String.format("Name: %s\n", this.name);
-        returnMessage += String.format("Age: %x\n", this.age);
+        returnMessage += String.format("Age: %d\n", this.age);
         returnMessage += String.format("Gender: %c\n", gender);
 
         //Returns the completed message
