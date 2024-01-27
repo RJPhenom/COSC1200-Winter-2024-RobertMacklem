e93498cb4db3adec6273c685e2805c7eaecbcb03
RJ <rj@macklem.ca>
2024-01-27 16:26:17 -0500

    Update Person.java
    Updated person.java with constructor, getters, setters, and one custom method as required by assignment instructions

--- Assignment 1/Person.java
+++ Assignment 1/Person.java
@@ -8,5 +8,53 @@
 //
 
 public class Person {
+    //Attributes
+    private String name;
+    private Integer age;
+    private Character gender;
+
+    //Constructor
+    public Person(String name, Integer age, Character gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    //Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
     
+    public Character getGender() {
+        return this.gender;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    //The introduce method works to return a formatted output
+    //of the information for the person instance.
+    public String introduce() {
+        //Prep return string var
+        String returnMessage = "";
+
+        //TODO: functionality
+
+        //Returns the completed message
+        return returnMessage;
+    }
 }
