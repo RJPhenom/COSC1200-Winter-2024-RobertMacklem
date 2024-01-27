df6e4095e2feb58610d7165a316ff9814d098022
RJ <rj@macklem.ca>
2024-01-27 16:12:31 -0500

    Add: init Assignment #1
    Added project folder for Assignment #1 and this is the initial commit for the requisite files (they've been created with classes and comment headers to start)

---
+++ Assignment 1/BurgerShop.java
@@ -0,0 +1,12 @@
+//  Author: Robert Macklem
+//  Course: COSC1200-01
+//  Date:   January 27, 2024
+//  Title:  Assignment 1: Part Two - "Burger Shop"
+//  Descr: 
+//
+//          Text
+//
+
+public class BurgerShop {
+    
+}
---
+++ Assignment 1/Calculator.java
@@ -0,0 +1,12 @@
+//  Author: Robert Macklem
+//  Course: COSC1200-01
+//  Date:   January 27, 2024
+//  Title:  Assignment 1: Part Three - "Calculator"
+//  Descr: 
+//
+//          Text
+//
+
+public class Calculator {
+    
+}
