bb1a585b44fbffc604f5e0ff20386069d4f595d3
RJ <rj@macklem.ca>
2024-03-16 20:54:50 -0400

    Update Accounts.java
    Add comment header

---
+++ Lab 3/Accounts.java
@@ -0,0 +1,7 @@
+//****************************************************************
+//Name:     Accounts.java  
+//Auth:     Robert Macklem
+//Date:     March 16 2024
+//Desc:     File that handles multiple bank account types using
+//          inheritance and polymorphism.
+//****************************************************************
